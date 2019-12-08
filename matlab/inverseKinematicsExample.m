79f642f3a23ea4b833009472cc00d83e554f992d
Matt Kennedy <mattkennedy416@gmail.com>
2019-12-08 15:42:04 -0500

    added inverse kinematics test

--- matlab/inverseKinematicsExample.m
+++ matlab/inverseKinematicsExample.m
@@ -1,23 +1,22 @@
 
-% calculate the pseudo-inverse kinematics using the iterative jacobian method
-% no analytical solution exists (I assume but didn't try) so need to find iteratively
-
-% note that deltaX must be small for this to converge
-
-% from Homework 5 problem 5
-
 
 clear
+
 
 %% set up state
 % fixed joints
 state.a2 = 0; % joint width, meters
-state.d6 = 0; % wrist length, meters
 state.a3 = 10;
 state.a4 = 10;
-state.a5 = 20;
 
-% variable joints
+state.d6 = 20; % wrist length, meters
+state.a5 = 0; % wrist length? 
+
+global pT
+pT = [0; 50; -12]; % point tool tip relative to origin 7
+
+
+% % variable joints - default state hanging out over ship
 state.d1 = 50;
 state.theta2 = 0;
 state.theta3 = pi/2;
@@ -26,103 +25,86 @@ state.theta5 = 0;
 state.theta6 = 0;
 state.theta7 = 0;
 
-% ship joints
-state.ship_yaw = 0;
-state.ship_pitch = 0;
-state.ship_roll = 0;
-state.arm_x = 1;
-state.arm_y = 3;
-jointRateLimits = [0.25; 0.1; 0.1; 0.25; 0.1; 0.1; 0.1];
+
+jointRateLimits = [1; 0.1; 0.1; 1; 0.1; 0.1; 0.1];
 
 
 state = dhParameters(state);
 
-global pT
-pT = [0; 50; -12]; % point tool tip relative to origin 7
 
-[J, J_tran] = Jacobian(state,pT);
-
+%% run the iterative jacobian
 
 initialJointPositions = forwardKinematics(state, pT, true);
 
+targetPosition = [65; 65; 65;];
+targetPitch = -pi/8;
+targetYaw = pi/8;
 
 
+intermediateJointPositions = initialJointPositions;
+for n = 1:25
 
-dt = 0.5;
+    P5T = intermediateJointPositions(:,9) - intermediateJointPositions(:,6);
+    
+    PGoal = targetPosition - P5T(1:3);
 
-% PGoalBase = [1.5; 1; 2.75];
-% PGoal = PGoalBase;
-PGoal = [50; 0; 75];
+    targetOrigin = 5;
 
-for steps = 1:50
+    % so actually our goal value is going to be a function of the goal? need to
+    % solve iteratively? ie the angle of tip relative to joint 5 is a function
+    % of where joint 5 is
+    [stateGoal, ~] = iterativeJacobian(state, PGoal, targetOrigin);
+    stateGoal.theta_i(5) = pi/2 - stateGoal.theta_i(3);
+    stateGoal.theta_i(6) = targetPitch;
+    stateGoal.theta_i(7) = targetYaw - stateGoal.theta_i(2);
+    intermediateJointPositions = forwardKinematics(stateGoal, pT, true);
     
-    %PGoal = PGoal + 4*[(rand(1)-0.5); (rand(1)-0.5); (rand(1)-0.5)];
-    
-    stateGoal = iterativeJacobian(state, PGoal);
-    state = takeStep(state, stateGoal, jointRateLimits, dt);
-    
-    newJointPositions = forwardKinematics(state, pT, true);
-    
-    tipPoints(steps,1:3) = newJointPositions(:,end);
-    
-    
-    % pull out the parameters that we want
-    time(steps,1) = dt*(steps-1); % need time!
-    d1(steps,1) = state.d_i(1);
-    theta2(steps,1) = state.theta_i(2);
-    theta3(steps,1) = state.theta_i(3);
-    d4(steps,1) = state.d_i(4);
-    theta5(steps,1) = state.theta_i(5);
-    theta6(steps,1) = state.theta_i(6);
-    theta7(steps,1) = state.theta_i(7);   
-    
-    
-    plot3(initialJointPositions(1,:), initialJointPositions(2,:), initialJointPositions(3,:));
-    hold on
-    plot3(newJointPositions(1,:), newJointPositions(2,:), newJointPositions(3,:), 'r');
-    plot3(PGoal(1),PGoal(2),PGoal(3), 'r*')
-    xlabel('x')
-    ylabel('y')
-    axis('equal')
+    if norm(intermediateJointPositions(:,9) - targetPosition) < 1E-2
+        break
+    end
 
 end
 
 
-T = table(time, d1, theta2, theta3, d4, theta5, theta6, theta7);
 
-makeRoboWorksAnimation(T, 'animationTest1');
+[stateGoal, iterativeJacobianError] = iterativeJacobian(state, PGoal, targetOrigin);
 
-
-
-
-function newState = takeStep(state, stateGoal, jointRateLimits, dt)
-% just linearly interpolate the joint positions?
+stateGoal.theta_i(5) = pi/2 - stateGoal.theta_i(3);
+stateGoal.theta_i(6) = targetPitch;
+stateGoal.theta_i(7) = targetYaw - stateGoal.theta_i(2);
 
 q = dh2q(state);
 qGoal = dh2q(stateGoal);
-qNew = zeros(size(q));
-
-qDeltaTotal = qGoal - q;
-qDeltaAllowable = jointRateLimits * dt;
-
-for n = 1:length(qDeltaTotal)
-   
-    if abs(qDeltaTotal(n)) <= qDeltaAllowable(n)
-        qNew(n) = qGoal(n); % are allowed to move this much this time step
-    else
-        qNew(n) = q(n) + qDeltaAllowable(n) * sign(qDeltaTotal(n)); % move in the wanted direction by the allowable delta       
-    end
-    
-end
-
-newState = q2state(qNew, state);
-
-end
 
 
+finalJointPositions = forwardKinematics(stateGoal, pT, true);
+
+steps = 1;
+d1(steps,1) = state.d_i(1);
+theta2(steps,1) = state.theta_i(2);
+theta3(steps,1) = state.theta_i(3);
+d4(steps,1) = state.d_i(4);
+theta5(steps,1) = state.theta_i(5);
+theta6(steps,1) = state.theta_i(6);
+theta7(steps,1) = state.theta_i(7); 
+
+steps = 2;
+d1(steps,1) = stateGoal.d_i(1);
+theta2(steps,1) = stateGoal.theta_i(2);
+theta3(steps,1) = stateGoal.theta_i(3);
+d4(steps,1) = stateGoal.d_i(4);
+theta5(steps,1) = stateGoal.theta_i(5);
+theta6(steps,1) = stateGoal.theta_i(6);
+theta7(steps,1) = stateGoal.theta_i(7); 
+
+time = [0; 1];
+
+armState = table(time, d1, theta2, theta3, d4, theta5, theta6, theta7);
+makeRoboWorksAnimation(armState, 'inverseKinematicsExample');
 
 
-function [newState] = iterativeJacobian(state, PGoal)
+
+function [newState, error] = iterativeJacobian(state, PGoal, targetOrigin)
 
 global pT
 
@@ -149,26 +131,28 @@ q = q_initial;
 
 state_temp = state;
 
-for iter = 1:4
+for iter = 1:10
     q_initial = q;
     
     jointPositions = forwardKinematics(state_temp, pT);
-    x_initial = jointPositions(:,8);
+    x_initial = jointPositions(:,targetOrigin);
     
     fprintf("LOOP %i =>  theta = (%f,%f,%f)\t\t p = (%f,%f,%f)\n", iter, q(1),q(2),q(3),x_initial(1),x_initial(2),x_initial(3));
     deltaX = PGoal - x_initial;
     
     % if DOF > 6 then use the pseudoinverse Jacobian!
-    [~, J_tran] = Jacobian(state_temp, pT);
+%     [~, J_tran] = Jacobian(state_temp, pT);
+    J_tran = J15_translation(state_temp);
     Jpi = pseudoInverseJacobian(J_tran);
     
 %     deltaQ = J_tran\deltaX; % equiv to inv(J)*deltaX
     deltaQ = Jpi * deltaX;
+    deltaQ(6:7) = [0;0];
     q = q_initial + deltaQ;
     
-    q(5) = pi/2 - q(3);
-    q(6) = 0; % this is the angle of the tip, want it to remain horizontal?
-    
+%     q(5) = pi/2 - q(3);
+%     q(6) = 0; % this is the angle of the tip, want it to remain horizontal?
+    error(iter) = norm(deltaQ);
     state_temp = q2state(q, state_temp); % update the state
 end
 
@@ -199,12 +183,12 @@ jointPositions = zeros(3,size(dh,1));
 T_total = eye(4);
 for link = 1:size(dh,1)
    T_total = T_total * Transform( dh.theta_i(link), dh.alpha_i1(link), dh.a_i1(link), dh.d_i(link) );
-   jointPositions(:,link) = transpose(T_total(1:3,4));
+   jointPositions(:,link) = T_total(1:3,4);
 end
 
 % and then to get the tool tip at the end
 % pT(3) becomes the d and pT(1) becomes the a? 
-T_total = T_total * Transform( 0, 0, pT(3), pT(1) );
+T_total = T_total * Transform( 0, 0, pT(3), pT(2) );
 jointPositions(:,link+1) = transpose(T_total(1:3,4));
 
 if nargin == 3
